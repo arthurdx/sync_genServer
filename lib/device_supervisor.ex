297c048c714d3efc4c112a19bf21d5770f23b90c
Arthur <arthurrochadx@gmail.com>
2026-03-10 19:47:27 -0300

    feat: dynamic supervisor for device sessions

---
+++ lib/device_supervisor.ex
@@ -0,0 +1,15 @@
+defmodule DeviceSupervisor do
+  use DynamicSupervisor
+
+  def start_link do
+    DynamicSupervisor.start_link(__MODULE__, [])
+  end
+
+  def init(_) do
+    DynamicSupervisor.init(strategy: :one_for_one)
+  end
+
+  def start_device(sup_pid, device_id) do
+    DynamicSupervisor.start_child(sup_pid, {DeviceSession, device_id})
+  end
+end
